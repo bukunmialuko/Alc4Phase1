660b2eeb9d0e54980ed5fb36700a73d6350da665
Daniel <oluwabukunmi.aluko@gmail.com>
2019-07-14 20:09:25 +0100

    Added progress bar.

--- app/src/main/java/com/andela/alc4phase1/AboutActivity1.java
+++ app/src/main/java/com/andela/alc4phase1/AboutActivity1.java
@@ -2,17 +2,24 @@
 
 package com.andela.alc4phase1;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.ProgressBar;
+
+import com.agrawalsuneet.loaderspack.loaders.RingAndCircleLoader;
 
 public class AboutActivity1 extends AppCompatActivity {
 
 
     private WebView mWebView;
+    private ProgressBar ringAndCircleLoader;
 
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,15 +38,33 @@ public class AboutActivity1 extends AppCompatActivity {
     private void initWidgets() {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        mWebView = findViewById(R.id.about_webview);
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        mWebView = findViewById(R.id.web_view);
+
+        ringAndCircleLoader =  findViewById(R.id.progress_bar);
+
+
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     private void bindWidgets() {
         WebSettings webSettings = mWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
-        mWebView.setWebViewClient(new MyAppWebViewClient(getApplicationContext()));
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NARROW_COLUMNS);
+        webSettings.setUseWideViewPort(true);
+
+        mWebView.setWebContentsDebuggingEnabled(false);
+        mWebView.getSettings().setAppCacheEnabled(true);
+        mWebView.getSettings().setSupportZoom(true);
+        mWebView.setHorizontalScrollBarEnabled(false);
+        mWebView.setVerticalScrollBarEnabled(false);
+
+
+        mWebView.setWebViewClient(new MyAppWebViewClient(getApplicationContext(), ringAndCircleLoader));
     }
 
 
--- app/src/main/java/com/andela/alc4phase1/MyAppWebViewClient.java
+++ app/src/main/java/com/andela/alc4phase1/MyAppWebViewClient.java
@@ -2,18 +2,28 @@ package com.andela.alc4phase1;
 
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.net.http.SslError;
+import android.view.View;
+import android.webkit.SslErrorHandler;
 import android.webkit.WebResourceError;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 import android.widget.Toast;
+
+import com.agrawalsuneet.loaderspack.loaders.RingAndCircleLoader;
 
 public class MyAppWebViewClient extends WebViewClient {
 
     private Context mContext;
+    private ProgressBar ringAndCircleLoader;
 
-    public MyAppWebViewClient(Context mContext) {
+
+    public MyAppWebViewClient(Context mContext, ProgressBar ringAndCircleLoader) {
         this.mContext = mContext;
+        this.ringAndCircleLoader = ringAndCircleLoader;
     }
 
     @Override
@@ -27,4 +37,23 @@ public class MyAppWebViewClient extends WebViewClient {
         // Redirect to deprecated method, so you can use it in all SDK versions
         onReceivedError(view, rerr.getErrorCode(), rerr.getDescription().toString(), req.getUrl().toString());
     }
+
+
+    @Override
+    public void onPageStarted(WebView view, String url, Bitmap favicon) {
+        super.onPageStarted(view, url, favicon);
+        ringAndCircleLoader.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void onPageFinished(WebView view, String url) {
+        super.onPageFinished(view, url);
+        ringAndCircleLoader.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+        handler.proceed();
+    }
+
 }
